5584dac38d95d2a9b3294df003507d243fdf6a89
shad one <info@shad-one.com>
2021-08-15 22:00:10 +0000

    Auto commit by CyberPanel Daily cron on 08-15-2021_22-00-09

--- admin/controller/extension/payment/tap.php
+++ admin/controller/extension/payment/tap.php
@@ -3,53 +3,39 @@ class ControllerExtensionPaymentTap extends Controller {
 	private $error = array();
 
 	public function index() {
-
 		$this->load->language('extension/payment/tap');
 
 		$this->document->setTitle($this->language->get('heading_title'));
-		$arr = array();	
-				
-		foreach($this->request->post as $key => $value)
-		{
-			if($key == 'payment_paytm_merchant2')
-			{				
-				 $arr[$key] = encrypt_e($value, $const1);		
-				 continue;
-			}
-			$arr[$key] = $value;
-		} 
 
 		$this->load->model('setting/setting');
 
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-			$this->model_setting_setting->editSetting('payment_tap', $this->request->post);
+			$this->model_setting_setting->editSetting('tap', $this->request->post);
 
 			$this->session->data['success'] = $this->language->get('text_success');
 
-			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true));
+			$this->response->redirect($this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL'));
 		}
 
 		$data['heading_title'] = $this->language->get('heading_title');
+		
 		$data['text_edit'] = $this->language->get('text_edit');
 		$data['text_enabled'] = $this->language->get('text_enabled');
 		$data['text_disabled'] = $this->language->get('text_disabled');
 		$data['text_all_zones'] = $this->language->get('text_all_zones');
 		$data['text_yes'] = $this->language->get('text_yes');
 		$data['text_no'] = $this->language->get('text_no');
-		$data['entry_test_secret_key'] = $this->language->get('entry_test_secret_key');
-		$data['entry_test_public_key'] = $this->language->get('entry_test_public_key');
 
-
-		$data['entry_secret_live_key'] = $this->language->get('entry_secret_live_key');
-		$data['entry_public_live_key'] = $this->language->get('entry_public_live_key');
+		$data['entry_merchantid'] = $this->language->get('entry_merchantid');
+		$data['entry_username'] = $this->language->get('entry_username');
+		$data['entry_password'] = $this->language->get('entry_password');
+		$data['entry_test'] = $this->language->get('entry_test');
 		$data['entry_total'] = $this->language->get('entry_total');
 		$data['entry_order_status'] = $this->language->get('entry_order_status');
 		$data['entry_geo_zone'] = $this->language->get('entry_geo_zone');
 		$data['entry_status'] = $this->language->get('entry_status');
-		
-		$data['entry_post_url'] = $this->language->get('entry_post_url');
-		$data['entey_charge_mode'] = $this->language->get('entry_charge_mode');
-        $data['entry_ui_mode'] = $this->language->get('entry_ui_mode');
+		$data['entry_sort_order'] = $this->language->get('entry_sort_order');
+
 		$data['help_password'] = $this->language->get('help_password');
 		$data['help_total'] = $this->language->get('help_total');
 
@@ -62,130 +48,82 @@ class ControllerExtensionPaymentTap extends Controller {
 			$data['error_warning'] = '';
 		}
 
-		if (isset($this->error['error_test_secret_key'])) {
-			$data['error_test_secret_key'] = $this->error['error_test_secret_key'];
+		if (isset($this->error['merchantid'])) {
+			$data['error_merchantid'] = $this->error['merchantid'];
 		} else {
-			$data['error_test_secret_key'] = '';
+			$data['error_merchantid'] = '';
 		}
 		
-		if (isset($this->error['error_test_public_key'])) {
-			$data['error_test_public_key'] = $this->error['error_test_public_key'];
+		if (isset($this->error['username'])) {
+			$data['error_username'] = $this->error['username'];
 		} else {
-			$data['error_test_public_key'] = '';
+			$data['error_username'] = '';
 		}
 
-		if (isset($this->error['error_secret_live_key'])) {
-			$data['error_secret_live_key'] = $this->error['error_secret_live_key'];
+		if (isset($this->error['password'])) {
+			$data['error_password'] = $this->error['password'];
 		} else {
-			$data['error_secret_live_key'] = '';
-		}
-		if (isset($this->error['erroe_post_url'])) {
-			$data['erroe_post_url'] = $this->error['erroe_post_url'];
-		} else {
-			$data['erroe_post_url'] = '';
+			$data['error_password'] = '';
 		}
 
 		$data['breadcrumbs'] = array();
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('text_home'),
-			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+			'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], 'SSL')
 		);
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('text_payment'),
-			'href' => $this->url->link('extension/payment',  'user_token=' . $this->session->data['user_token'], true)
+			'href' => $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL')
 		);
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('heading_title'),
-			'href' => $this->url->link('payment/tap', 'user_token=' . $this->session->data['user_token'], true)
+			'href' => $this->url->link('extension/payment/tap', 'token=' . $this->session->data['token'], 'SSL')
 		);
 
-		$data['action'] = $this->url->link('extension/payment/tap', 'user_token=' . $this->session->data['user_token'], true);
+		$data['action'] = $this->url->link('extension/payment/tap', 'token=' . $this->session->data['token'], 'SSL');
 
-		$data['cancel'] = $this->url->link('extension/payment',  'user_token=' . $this->session->data['user_token'], true);
+		$data['cancel'] = $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL');
 
-		if (isset($this->request->post['payment_tap_test_secret_key'])) {
-			$data['payment_tap_test_secret_key'] = $this->request->post['payment_tap_test_secret_key'];
+		if (isset($this->request->post['tap_merchantid'])) {
+			$data['tap_merchantid'] = $this->request->post['tap_merchantid'];
 		} else {
-			$data['payment_tap_test_secret_key'] = $this->config->get('payment_tap_test_secret_key');
+			$data['tap_merchantid'] = $this->config->get('tap_merchantid');
 		}
 		
-		if (isset($this->request->post['payment_tap_test_public_key'])) {
-			$data['payment_tap_test_public_key'] = $this->request->post['payment_tap_test_public_key'];
+		if (isset($this->request->post['tap_username'])) {
+			$data['tap_username'] = $this->request->post['tap_username'];
 		} else {
-			$data['payment_tap_test_public_key'] = $this->config->get('payment_tap_test_public_key');
-		}
-		if (isset($this->request->post['payment_tap_secret_live_key'])) {
-			$data['payment_tap_secret_live_key'] = $this->request->post['payment_tap_secret_live_key'];
-		} else {
-			$data['payment_tap_secret_live_key'] = $this->config->get('payment_tap_secret_live_key');
+			$data['tap_username'] = $this->config->get('tap_username');
 		}
 
-		if (isset($this->request->post['payment_tap_public_live_key'])) {
-			$data['payment_tap_public_live_key'] = $this->config->get('payment_tap_public_live_key');
-		// } else {
-		// 	$data['payment_tap_password'] = md5(mt_rand());
-		 }
-		else {
-			$data['payment_tap_public_live_key'] = $this->config->get('payment_tap_public_live_key');
+		if (isset($this->request->post['tap_password'])) {
+			$data['tap_username'] = $this->request->post['tap_username'];
+		} elseif ($this->config->get('tap_password')) {
+			$data['tap_password'] = $this->config->get('tap_password');
+		} else {
+			$data['tap_password'] = md5(mt_rand());
 		}
 
 		if (isset($this->request->post['tap_test'])) {
-			$data['payment_tap_test'] = $this->request->post['payment_tap_test'];
+			$data['tap_test'] = $this->request->post['tap_test'];
 		} else {
-			$data['payment_tap_test'] = $this->config->get('payment_tap_test');
+			$data['tap_test'] = $this->config->get('tap_test');
 		}
 
-		if (isset($this->request->post['payment_tap_total'])) {
-			$data['payment_tap_total'] = $this->request->post['payment_tap_total'];
+		if (isset($this->request->post['tap_total'])) {
+			$data['tap_total'] = $this->request->post['tap_total'];
 		} else {
-			$data['payment_tap_total'] = $this->config->get('payment_tap_total');
+			$data['tap_total'] = $this->config->get('tap_total');
 		}
 
-		if (isset($this->request->post['payment_tap_order_status_id'])) {
-			$data['payment_tap_order_status_id'] = $this->request->post['payment_tap_order_status_id'];
+		if (isset($this->request->post['tap_order_status_id'])) {
+			$data['tap_order_status_id'] = $this->request->post['tap_order_status_id'];
 		} else {
-			$data['payment_tap_order_status_id'] = $this->config->get('payment_tap_order_status_id');
+			$data['tap_order_status_id'] = $this->config->get('tap_order_status_id');
 		}
-		if (isset($this->request->post['payment_tap_post_url'])) {
-			$data['payment_tap_post_url'] = $this->request->post['payment_tap_post_url'];
-		} else {
-			$data['payment_tap_post_url'] = $this->config->get('payment_tap_post_url');
-		}
-
-
-
-
-
-        
-        // Arry
-      //  $data['chargemodes'] = array("Charge", "Authorize");
-        //var_dump($data['chargemodes']);exit;
-
-        
-        
-     
-       
-    
-
-		if (isset($this->request->post['payment_tap_charge_mode'])) {
-			$data['payment_tap_charge_mode'] = $this->request->post['payment_tap_charge_mode'];
-		} else {
-			$data['payment_tap_charge_mode'] = $this->config->get('payment_tap_charge_mode');
-		}
-
-		if (isset($this->request->post['payment_tap_ui_mode'])) {
-			$data['payment_tap_ui_mode'] = $this->request->post['payment_tap_ui_mode'];
-		} else {
-			$data['payment_tap_ui_mode'] = $this->config->get('payment_tap_ui_mode');
-		}
-		
-
-
-
-
 
 		$this->load->model('localisation/order_status');
 
@@ -201,68 +139,41 @@ class ControllerExtensionPaymentTap extends Controller {
 
 		$data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
 
-		if (isset($this->request->post['payment_tap_status'])) {
-			$data['payment_tap_status'] = $this->request->post['payment_tap_status'];
+		if (isset($this->request->post['tap_status'])) {
+			$data['tap_status'] = $this->request->post['tap_status'];
 		} else {
-			$data['payment_tap_status'] = $this->config->get('payment_tap_status');
+			$data['tap_status'] = $this->config->get('tap_status');
 		}
 
-
-		    
-		
+		if (isset($this->request->post['tap_sort_order'])) {
+			$data['tap_sort_order'] = $this->request->post['tap_sort_order'];
+		} else {
+			$data['tap_sort_order'] = $this->config->get('tap_sort_order');
+		}
 
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 
-		$this->response->setOutput($this->load->view('extension/payment/tap', $data));
+		$this->response->setOutput($this->load->view('extension/payment/tap.tpl', $data));
 	}
-	public function install() {
-        $this->load->model('extension/payment/tap');
-    }
-
-    public function uninstall() {
-        $this->load->model('extension/payment/tap');
-    }
-
 
 	protected function validate() {
 		if (!$this->user->hasPermission('modify', 'extension/payment/tap')) {
 			$this->error['warning'] = $this->language->get('error_permission');
 		}
 
-		if (!$this->request->post['payment_tap_test_secret_key']) {
-			$this->error['payment_tap_test_secret_key'] = $this->language->get('error_payment_tap_test_secret_key');
+		if (!$this->request->post['tap_merchantid']) {
+			$this->error['merchantid'] = $this->language->get('error_merchantid');
 		}
 		
-		if (!$this->request->post['payment_tap_test_public_key']) {
-			$this->error['payment_tap_test_public_key'] = $this->language->get('error_payment_tap_test_public_key');
+		if (!$this->request->post['tap_username']) {
+			$this->error['username'] = $this->language->get('error_username');
 		}
 
-		if (!$this->request->post['payment_tap_secret_live_key']) {
-			$this->error['payment_tap_secret_live_key'] = $this->language->get('error_payment_tap_secret_live_key');
+		if (!$this->request->post['tap_password']) {
+			$this->error['password'] = $this->language->get('error_password');
 		}
-
-		if (!$this->request->post['payment_tap_public_live_key']) {
-			$this->error['payment_tap_public_live_key'] = $this->language->get('error_payment_tap_public_live_key');
-		}
-
-        if (!$this->request->post['payment_tap_post_url']) {
-			$this->error['payment_tap_post_url'] = $this->language->get('erroe_post_url');
-		}
-
-
-		if (!$this->request->post['payment_tap_charge_mode']) {
-			$this->error['payment_tap_charge_mode'] = $this->language->get('error_charge_mode');
-		}
-
-		if (!$this->request->post['payment_tap_ui_mode']) {
-			$this->error['payment_tap_ui_mode'] = $this->language->get('error_ui_mode');
-		}
-
-
-		
-
 
 		return !$this->error;
 	}
--- admin/language/en-gb/extension/payment/tap.php
+++ admin/language/en-gb/extension/payment/tap.php
@@ -9,18 +9,15 @@ $_['text_edit']                     = 'Edit Tap';
 $_['text_tap']						= '<img src="view/image/payment/tap.png" alt="Tap" title="Tap" style="border: 1px solid #EEEEEE;" />';
 
 // Entry
-$_['entry_test_secret_key']				= 'Test Secret Key';
-$_['entry_test_public_key']				= 'Test public key';
-$_['entry_secret_live_key']		        = 'Live Secret key';
-$_['entry_public_live_key']	            = 'Live public Key';
-$_['entey_post_url']                    = 'Post Url';
-$_['entry_charge_mode']                 = 'charge Mode';
-$_['entry_ui_mode']                     = 'UI Mode';
-$_['entry_test']					    = 'Test Mode';
-$_['entry_total']					    = 'Total';
-$_['entry_order_status']			    = 'Order Status';
-$_['entry_geo_zone']				    = 'Geo Zone';
-$_['entry_status']					    = 'Status';
+$_['entry_merchantid']				= 'Tap Merchant ID';
+$_['entry_username']				= 'Tap Username';
+$_['entry_password']				= 'Password';
+$_['entry_test']					= 'Test Mode';
+$_['entry_total']					= 'Total';
+$_['entry_order_status']			= 'Order Status';
+$_['entry_geo_zone']				= 'Geo Zone';
+$_['entry_status']					= 'Status';
+$_['entry_sort_order']				= 'Sort Order';
 
 // Help
 $_['help_password']					= 'Just use some random password. This will be used to make sure the payment information is not interfered with after being sent to the payment gateway.';
@@ -28,9 +25,6 @@ $_['help_total']					= 'The checkout total the order must reach before this paym
 
 // Error
 $_['error_permission']				= 'Warning: You do not have permission to modify payment Tap!';
-$_['error_secret_live_key']				= 'Test secret live key!';
-$_['error__public_live_key']			= 'Test Public Live Key!';
-$_['error_test_secret_key']				= 'Test Sectret Key Requiredc!';
-$_['erroe_post_url']                    = ' Post Url';
-$_['error_charge_mode']                 = ' select charge mode';
-$_['error_ui_mode']                     = ' select Ui mode';
+$_['error_username']				= 'Tap Username required!';
+$_['error_password']				= 'Password required!';
+$_['error_merchantid']				= 'Tap Merchant ID required!';
